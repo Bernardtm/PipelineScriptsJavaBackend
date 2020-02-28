e9c02992029426dd49b2b78eb0ec0e52fdc92e59
Bernard Mendes <tr698476@oi.corp.net>
2020-02-28 11:21:26 -0300

    secret test

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,31 +3,38 @@ package hello;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping
     public String index() throws SecurityException, IOException {
-    	Logger logger = Logger.getLogger("HelloController");  
+    	// Logger logger = Logger.getLogger("HelloController");  
     	
-    	FileHandler fh;  
+    	// FileHandler fh;  
     	// This block configure the logger with handler and formatter  
-        fh = new FileHandler("/logs/console.log");  
-        logger.addHandler(fh);
-        SimpleFormatter formatter = new SimpleFormatter();  
-        fh.setFormatter(formatter);  
+        // fh = new FileHandler("/logs/console.log");  
+        // logger.addHandler(fh);
+        // SimpleFormatter formatter = new SimpleFormatter();  
+        // fh.setFormatter(formatter);  
 
         // the following statement is used to log any messages  
-        logger.info("Greetings from Spring Boot!");  
-        fh.close();
-    	
-        return "Greetings from Spring Boot!";
+        // logger.info("Greetings from Spring Boot!");  
+        // fh.close();
+        //Read environmental variables:
+        Map<String, String> env = System.getenv();
+        String secret = env.get("mySecret");
+        return secret != null ? secret : "Secret not found!";
     }
+
+    @GetMapping(value = "/hello")
+	public String hello() {
+		return "Hello World";
+	}
+
 
 }
